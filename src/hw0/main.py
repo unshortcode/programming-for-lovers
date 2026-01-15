4559e6499d6c914f8d7ef05e6eac028ae8d636e1
Long Nguyen <bio.longnguyen@gmail.com>
2026-01-15 08:09:57 +0700

    Implement a function Combination()

--- src/hw0/main.py
+++ src/hw0/main.py
@@ -15,6 +15,15 @@ def Permutation(n: int, k: int) -> int:
     return int(factorial(n) / factorial(n - k))
 
 def factorial(n: int) -> int:
+    """
+    Takes as input an integer n and returns n! = 1*2*...*(n-2)*(n-1)*n using a for loop.
+
+    Parameters:
+    - n (int): an integer
+    
+    Returns:
+    int: n! = 1*2*n*(n-1)*(n-2)*...*2*1
+    """
     if n < 0:
         raise ValueError
     p = 1
@@ -22,8 +31,24 @@ def factorial(n: int) -> int:
         p *= i
     return p
 
+# Implement a function Combination()
+def Combination(n: int, k: int) -> int:
+    """
+    Give n and k, calculate the Combination statistic P(n, k)
+
+    Parameters:
+    - n (int): the total number of objects in the set
+    - k (int): the number of objects to be unordered (0 <= k <= n)
+
+    Returns:
+    int: the total number of Combination, calculated as n! / ((n - k) * k!)!
+    """
+    if k < 0 or k > n:
+        raise ValueError
+    return int(factorial(n) / (factorial(n - k) * factorial(k)))
+
 def main():
-    print(Permutation(10, 0))
+    print(Combination(8, 7))
 
 if __name__ == "__main__":
     main()
