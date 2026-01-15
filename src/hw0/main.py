70f9ce0d7408f673ee83699d752c6e0d112eec27
Long Nguyen <bio.longnguyen@gmail.com>
2026-01-15 08:03:52 +0700

    Implement a function Permutation()

---
+++ src/hw0/main.py
@@ -0,0 +1,29 @@
+# Implement a function Permutation()
+def Permutation(n: int, k: int) -> int:
+    """
+    Give n and k, calculate the Permutation statistic P(n, k)
+
+    Parameters:
+    - n (int): the total number of objects in the set
+    - k (int): the number of objects to be ordered (0 <= k <= n)
+
+    Returns:
+    int: the total number of Permutations, calculated as n! / (n - k)!
+    """
+    if k < 0 or k > n:
+        raise ValueError
+    return int(factorial(n) / factorial(n - k))
+
+def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError
+    p = 1
+    for i in range(1, n + 1):
+        p *= i
+    return p
+
+def main():
+    print(Permutation(10, 0))
+
+if __name__ == "__main__":
+    main()
